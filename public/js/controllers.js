f0c14da400fb385913844b66834aad3d855c591c
Edward Raison <edwardraison@Edwards-MacBook-Pro.local>
2015-03-06 15:36:41 -0500

    Fixed loading message mechanism by enclosing everything in the controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,17 +6,13 @@ var weathermanControllers = angular.module('weathermanControllers', []);
 
 weathermanControllers.controller('HomeCtrl', ['$scope', '$http', 'geolocation',
     function($scope, $http, geolocation) {
-    	$scope.loaded = false;
+    	$scope.loading = true;
         geolocation.getLocation().then(function(data) {
-            $scope.coords = {
-                lat: data.coords.latitude,
-                long: data.coords.longitude
-            };
 
             $http.get('/api/conditions/' + data.coords.latitude + "," + data.coords.longitude).
             success(function(data, status, headers, config) {
                 $scope.conditions = data.current_observation;
-                $scope.loaded = true;
+                $scope.loading = false;
             }).
             error(function(data, status, headers, config) {
                 console.log(data);
